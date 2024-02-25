ff5ab023d597a7568787eb53e101f58533773733
Alexander Mendivil <alexanderima@outlook.es>
2024-02-24 17:48:51 -0700

    muzzle particles

--- weapons/hit_scan_weapon.gd
+++ weapons/hit_scan_weapon.gd
@@ -3,6 +3,7 @@ extends Node3D
 @onready var cooldown_timer:Timer = $CooldownTimer
 @onready var weapon_position: Vector3 = weapon_mesh.position
 @onready var ray_cast_3d:RayCast3D = $RayCast3D
+@onready var muzzle_flash:GPUParticles3D = $MuzzleFlash
 
 @export var fire_rate: float = 14.0
 @export var recoil: float = 0.05
@@ -17,11 +18,11 @@ func _process(delta):
 	if Input.is_action_pressed('fire'):
 		if cooldown_timer.is_stopped():
 			shoot()
-
 	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, (delta * 10.0))
 
 
 func shoot() -> void:
+	muzzle_flash.restart()
 	cooldown_timer.start(1.0 / fire_rate)
 	weapon_mesh.position.z += recoil
 	var collider: Object = ray_cast_3d.get_collider()
