8972f08f7265b6db32f939e6357d553732d19f27
Alexander Mendivil <alexanderima@outlook.es>
2024-03-11 18:18:13 -0700

    wheel to change weapon

--- player/WeaponHandler.gd
+++ player/WeaponHandler.gd
@@ -21,3 +21,23 @@ func _unhandled_input(event: InputEvent) -> void:
 		equip(weapon_1)
 	if event.is_action_pressed("weapon_2"):
 		equip(weapon_2)
+	if event.is_action_pressed("next_weapon"):
+		next_weapon()
+	if event.is_action_pressed("last_weapon"):
+		last_weapon()
+
+func next_weapon() -> void:
+	var index: int = get_current_index()
+	index = wrapi(index+1, 0, get_child_count()) 
+	equip(get_child(index))
+
+func last_weapon() -> void:
+	var index: int = get_children().size() - 1
+	index = wrapi(index - 1, 0, get_child_count()) 
+	equip(get_child(index))
+
+func get_current_index() -> int:
+	for i in get_child_count():
+		if get_child(i).visible:
+			return i
+	return 0
