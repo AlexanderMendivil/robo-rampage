8e8629f890653647b05ec2b598f3f3681944ef17
Alexander Mendivil <alexanderima@outlook.es>
2024-03-16 15:13:43 -0700

    pickup scene and animation

---
+++ pickups/Pickup.gd
@@ -0,0 +1,16 @@
+extends Area3D
+@export var ammo_type: AmmoHandler.ammo_type
+@export var ammo_amount: int = 10
+@onready var animation_player = $AnimationPlayer
+
+
+func _ready():
+	animation_player.play("glowing")
+
+func _on_body_entered(body:Node3D):
+	if body.is_in_group("PLAYER"):
+		queue_free()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	animation_player.play("glowing")
